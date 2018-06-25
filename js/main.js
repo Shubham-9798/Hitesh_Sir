c36f23da896f1f341f1c3a12aa7a1e47a0ffd279
programmer-kapoor <shubhamkapoor9798@gmail.com>
2018-06-24 20:19:34 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function(){
+    $('#toogle-btn').hide();
+
+
+
+
+
+
+//  $(window).resize(function() {
+//             var w = window.innerWidth
+//             || document.documentElement.clientWidth
+//             || document.body.clientWidth;
+            
+//             var h = window.innerHeight
+//             || document.documentElement.clientHeight
+//             || document.body.clientHeight;
+
+//             if(w<=500)   
+//             {
+//                //$('#toogle').hide();
+//                //$("#div1").fadeToggle();
+//                $('#toogle-btn').show();
+//                //$('#toogle')
+
+//                $("#toogle-btn").click(function(){
+//                 $("#toogle").toggle(2000);
+//                });
+//             }else{
+//                 $('#toogle-btn').hide();
+//             }
+
+//     });
+
+
+});
