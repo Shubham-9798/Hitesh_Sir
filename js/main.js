14fa5bf41f48264f8e1716b9ea6cd1dfe6b32b16
programmer-kapoor <shubhamkapoor9798@gmail.com>
2018-06-25 23:43:22 -0700

    third commit

--- js/main.js
+++ js/main.js
@@ -1,39 +1,27 @@
 
 $(document).ready(function(){
-    $('#toogle-btn').hide();
-
-
+ 
   var code=$('#edit').text();
+  // alert(code);
+  code=code.trim();
   console.log(typeof code);
   console.log(code.length);
-//   document.write(code);
-//  alert(code);
 
+  //for(let i=0;i<code.length;i++){
+    var str="";
+    var pos1= code.indexOf("/*");
+    var pos2= code.indexOf("*/");
+    for(var i=pos1;i<=pos2+1;i++)
+      {
+          str=str+code[i];
+          console.log(str);
+      }
 
-//  $(window).resize(function() {
-//             var w = window.innerWidth
-//             || document.documentElement.clientWidth
-//             || document.body.clientWidth;
-            
-//             var h = window.innerHeight
-//             || document.documentElement.clientHeight
-//             || document.body.clientHeight;
+ // }
+  console.log(str);
+  // console.log(pos2);
 
-//             if(w<=500)   
-//             {
-//                //$('#toogle').hide();
-//                //$("#div1").fadeToggle();
-//                $('#toogle-btn').show();
-//                //$('#toogle')
-
-//                $("#toogle-btn").click(function(){
-//                 $("#toogle").toggle(2000);
-//                });
-//             }else{
-//                 $('#toogle-btn').hide();
-//             }
-
-//     });
+ 
 
 
 });
